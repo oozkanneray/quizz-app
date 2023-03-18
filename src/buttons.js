c72aa007b66456fa1dcc9846cc5c70d8b34e7805
ebaytheway <lafleurr4141@gmail.com>
2023-03-18 20:08:32 +0300

    add: question, active button,user select

--- src/buttons.js
+++ src/buttons.js
@@ -1,5 +1,17 @@
+import { useState } from "react"
+
 export default function Buttons(props){
+
+    
+
     return(
-        <button className="btn">{props.btn}</button>
+        <button 
+        style={props.item.userSelect == props.btn ? {color:"green"} : {color:"black"}}
+        onClick={()=>{
+            props.item.userSelect = props.btn;
+            props.setActive(props.btn)
+            console.log(props.item.userSelect)
+        }} 
+        className="btn">{props.btn}</button>
     )
 }
