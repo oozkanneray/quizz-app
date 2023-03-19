0e4155686496c9808f43264f7e6dad1cf3f4f0e5
ebaytheway <lafleurr4141@gmail.com>
2023-03-20 01:12:26 +0300

    .

--- src/CharCard.js
+++ src/CharCard.js
@@ -1,28 +1,26 @@
-import "./assets/result.css"
+import "./assets/result.css";
 
 function Character(props) {
-    const resetGame = () => {
-        props.txt.map(item => item.userSelect = "")
-        props.setTab(0)
-        props.setNumber(1)
-        
-    }
-    return (
- 
-        <div className="result-container">
-            <div className="result-title">You're {props.item.name}</div>
-            <img className="result-img" src={props.item.src}></img>
-            <div className="result-text">{props.item.text} </div>
-            <button
-            className="result-btn"
-            onClick={()=>{
-                resetGame()
-            }}
-            >
-                Restart Quiz
-            </button>
-        </div>
-     );
+  const resetGame = () => {
+    props.txt.map((item) => (item.userSelect = ""));
+    props.setTab(0);
+    props.setNumber(1);
+  };
+  return (
+    <div className="result-container">
+      <div className="result-title">You're {props.item.name}</div>
+      <img className="result-img" src={props.item.src}></img>
+      <div className="result-text">{props.item.text} </div>
+      <button
+        className="result-btn"
+        onClick={() => {
+          resetGame();
+        }}
+      >
+        Restart Quiz
+      </button>
+    </div>
+  );
 }
 
 export default Character;
